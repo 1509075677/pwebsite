fa6c340d765eedf363ba4b2b3a89a8fbc5869cf4
1509075677 <jchen386@buffalo.edu>
2022-06-30 23:53:31 -0400

    icons linked to websites

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  function toinstagram(){
+    window.location="https://www.instagram.com/jasxv9/";
+  }
+  function tolinkedin(){
+    window.location="https://www.linkedin.com/in/jason-chen-395523221/";
+  }
+  function togithub(){
+    window.location="https://github.com/1509075677";
+  }
   return (
-    <div className="App">
+    <div class="App">
       
       <div class="landingpfp"><img src={require('./images/landing/landingpfp.svg').default}/></div>
       <div class="landingquoteup"><img src={require('./images/landing/landingquoteup.svg').default}/></div>
@@ -17,9 +26,9 @@ function App() {
         <div class="pelements"><img src={require('./images/landing/landingprojects.svg').default}/></div>
       </div>
       <div class='icon'>
-      <div class="iconp"><img src={require('./images/landing/landinggithub.svg').default}/></div>
-      <div class="iconp"><img src={require('./images/landing/landinginstagram.svg').default}/></div>
-      <div class="iconp"><img src={require('./images/landing/landinglinkedin.svg').default}/></div>
+      <div class="iconp" onClick={togithub}><img src={require('./images/landing/landinggithub.svg').default}/></div>
+      <div class="iconp" onClick={toinstagram}><img src={require('./images/landing/landinginstagram.svg').default}/></div>
+      <div class="iconp" onClick={tolinkedin}><img src={require('./images/landing/landinglinkedin.svg').default}/></div>
       </div>
     </div>
   );
