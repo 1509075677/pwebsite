6f976838541c9bda7ba5f4719e258c003c59ffdb
1509075677 <jchen386@buffalo.edu>
2022-07-10 20:39:43 -0400

    Added a contacts page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
 import React from 'react';
-import './App.css';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Contacts from './contacts';
+import Landing from './landing';
 
 function App() {
-  function toinstagram(){
-    window.location="https://www.instagram.com/jasxv9/";
-  }
-  function tolinkedin(){
-    window.location="https://www.linkedin.com/in/jason-chen-395523221/";
-  }
-  function togithub(){
-    window.location="https://github.com/1509075677";
-  }
-  function toresume(){
-    window.location="https://drive.google.com/file/d/1uk7q6qIARsacXUagzf7K8fTx7tMaO2l2/view";
-  }
   return (
-    <div class="App">
-      
-      <div class="landingpfp"><img src={require('./images/landing/landingpfp.svg').default}/></div>
-      <div class="landingquoteup"><img src={require('./images/landing/landingquoteup.svg').default}/></div>
-      <div class="landingquotedown"><img src={require('./images/landing/landingquotedown.svg').default}/></div>
-      <div class="landingline"><img src={require('./images/landing/landingline.svg').default}/></div>
-      
-      <div class='directory'>
-        <div class="elements"><img src={require('./images/landing/landingabout.svg').default}/></div>
-        <div class="elements" onClick={toresume}><img src={require('./images/landing/landingresume.svg').default}/></div>
-        <div class="elements"><img src={require('./images/landing/landingcontact.svg').default}/></div>
-        <div class="pelements"><img src={require('./images/landing/landingprojects.svg').default}/></div>
-      </div>
-      <div class='icon'>
-      <div class="iconp" onClick={togithub}><img src={require('./images/landing/landinggithub.svg').default}/></div>
-      <div class="iconp" onClick={toinstagram}><img src={require('./images/landing/landinginstagram.svg').default}/></div>
-      <div class="iconp" onClick={tolinkedin}><img src={require('./images/landing/landinglinkedin.svg').default}/></div>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/pwebsite" element={<Landing />}/>
+        <Route path="/contacts" element={<Contacts />}/>
+      </Routes>
+    </Router>
   );
 }
 
